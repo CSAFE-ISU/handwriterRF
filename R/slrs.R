abb8f953cc5688ad7c379e46403df3fc81e9c1af
Stephanie Reinders <reinders.stephanie@gmail.com>
2024-09-19 12:37:11 -0500

    Created function to calculate SLR for two input handwriting samples

--- R/slrs.R
+++ R/slrs.R
@@ -1,17 +1,21 @@
-eval_density_at_point <- function(density, x){
-  evals <- list()
-  evals$numerators <- approx(density$same_writer$x, density$same_writer$y, xout = x, n=10000)$y
-  evals$denominators <- approx(density$diff_writer$x, density$diff_writer$y, xout = x, n=10000)$y
-  return(evals)
+calculate_slr <- function(score, pdfs = densities){
+  numerator <- eval_density_at_point(den = pdfs$same_writer, x = score)
+  denominator <- eval_density_at_point(den = pdfs$diff_writer, x = score)
+  return(numerator / denominator)
 }
 
-correct_NAs <- function(evals, zero_correction = 1e-10){
-  evals$numerators[is.na(evals$numerators)] <- 0
-  evals$denominators[is.na(evals$denominators)] <- zero_correction
-  return(evals)
+eval_density_at_point <- function(den, x){
+  y <- approx(den$x, den$y, xout = x, n=10000)$y
+  return(y)
 }
 
-correct_zeros <- function(evals, zero_correction = 1e-10){
-  evals$denominators[which(evals$denominators == 0)] <- zero_correction
-  return(evals)
-}
+# correct_NAs <- function(evals, zero_correction = 1e-10){
+#   evals$numerators[is.na(evals$numerators)] <- 0
+#   evals$denominators[is.na(evals$denominators)] <- zero_correction
+#   return(evals)
+# }
+#
+# correct_zeros <- function(evals, zero_correction = 1e-10){
+#   evals$denominators[which(evals$denominators == 0)] <- zero_correction
+#   return(evals)
+# }
