bf208569a96f605dd37adacdc8111c5fd33348d0
Stephanie Reinders <reinders.stephanie@gmail.com>
2025-01-10 09:29:37 -0600

    Replaced previous template with Amy Crawford's template
    I decided to replace the previous cluster template with the one made by Amy Crawford so that results reported in the handwriter model card will all be based on the same template.

--- data-raw/make_template.R
+++ data-raw/make_template.R
@@ -1,6 +1,3 @@
-templateK40 <- readRDS(file.path("data-raw", "templateK40.rds"))
-# delete graphs to reduce file size
-templateK40$template_graphs <- NULL
-# delete all wcd except for last iteration to reduce file size
-templateK40$wcd <- templateK40$wcd[templateK40$iters, ]
+# Use the template from handwriter.
+templateK40 <- handwriter::templateK40
 usethis::use_data(templateK40, overwrite = TRUE)
