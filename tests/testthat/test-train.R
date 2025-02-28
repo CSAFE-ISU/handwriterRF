a1be24bd2f085d1aeb3f84820c175540e3b20d73
Stephanie Reinders <reinders.stephanie@gmail.com>
2025-02-28 11:44:16 -0600

    feat(train): add support for training random forest on distances between two dataframes
    train_rf() function now accepts an optional second dataframe parameter. When provided, train_rf() calculates distances between all pairs of rows from the first and second dataframes, rather than within a single dataframe. Then, a random forest is trained on the distances. The primary use case for this is to train a random forest on distances between pairs of handwriting samples where one sample is short and the other is long, without also calculating the distances between two long samples or two short samples.

--- tests/testthat/test-train.R
+++ tests/testthat/test-train.R
@@ -1,4 +1,4 @@
-test_that("Train random forest works with ranger package", {
+test_that("Train random forest works with ranger package and 1 dataframe", {
   # Despite setting the seed, the random forest created on Windows has
   # reasonable values but is not equal to the fixture random forest created on
   # Stephanie's Mac. I tried using skip_on_os(os = "windows", arch = NULL), but
@@ -14,3 +14,22 @@ test_that("Train random forest works with ranger package", {
     downsample = TRUE
   ), NA)
 })
+
+test_that("Train random forest works with ranger package and 2 dataframes", {
+  # Despite setting the seed, the random forest created on Windows has
+  # reasonable values but is not equal to the fixture random forest created on
+  # Stephanie's Mac. I tried using skip_on_os(os = "windows", arch = NULL), but
+  # this test still returned an error when CRAN ran automated tests on Debian.
+  # As a work around, test that the function runs without error.
+
+  expect_error(train_rf(
+    df = train[1:50, ],
+    ntrees = 200,
+    distance_measures = "euc",
+    df2 = train[51:100, ],
+    output_dir = NULL,
+    run_number = 1,
+    downsample = TRUE
+  ), NA)
+})
+
